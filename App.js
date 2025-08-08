54ed3307ef94a6e2665ada90a763ae2f6226631b
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-08-08 14:36:24 +0000

    feat : add get methods and feature

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const characters = require("./characters.json");
+const marvelData = require("./characters.json");
+
+app.use(express.json());
 
 // get all characters
 app.get("/characters", (req, res) => {
   try {
-    res.json(characters);
+    res.json(marvelData);
   } catch (error) {
     res.status(500).json({ error: "Error proceding request" });
+  }
+});
+
+// get a character by id
+app.get("/characters/:id", (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    const character = marvelData.characters.find((item) => item.id === id);
+
+    if (!character) {
+      return res.status(404).json({ message: "Character not found" });
+    }
+
+    res.json(character);
+  } catch (error) {
+    res.status(500).json({ message: "Error proceding request" });
   }
 });
 
